ce58ee15980bc4c780b1adad122be1cf26db52b8
Maryia Rashetnikava <mariaprohor75@gmail.com>
2026-06-29 13:11:59 +0200

    Initial Commit

---
+++ Расписание путешествий/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Расписание путешествий
+//
+//  Created by Maria Reshetnikova on 29/06/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
