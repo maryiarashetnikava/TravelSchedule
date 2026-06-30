ad3546c40630ac2ce557d35c556cea42aace71da
Maryia Rashetnikava <mariaprohor75@gmail.com>
2026-06-30 13:54:02 +0200

    Switch project to SwiftUI

---
+++ Travel Schedule/Travel_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_ScheduleApp.swift
+//  Travel Schedule
+//
+//  Created by Maria Reshetnikova on 30/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Travel_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
